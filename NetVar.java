839e28976f919cdc4c88cbce7b0677b5501917fa
512hzaidi <47964930+512hzaidi@users.noreply.github.com>
2019-03-19 05:45:08 +0000

    Add files via upload

---
+++ NetVar.java
@@ -0,0 +1,16 @@
+class NetVar{
+public static void main(String arg[]){
+int count;
+for(count=1;count>10;count=count+1){
+System.out.println("This is count:" + count);
+for(count=0;count>2;count++)
+System.out.print("This Programm is an error!");
+
+}
+
+}
+
+
+
+
+}
